ed0e6c8f4aa5baf093b69a09121f88af1bc409e6
prithvi <prithvi08242@gmail.com>
2026-07-17 11:56:08 -0500

    unit test

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,14 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+
   test: {
-    globals: true,
     environment: "jsdom",
+    globals: true,
     setupFiles: "./src/test/setup.ts",
   },
 });
